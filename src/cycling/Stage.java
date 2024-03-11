19587933f4aa2f39b524ace43c3dcf11243e6629
Negativity7477 <118772061+Negativity7477@users.noreply.github.com>
2024-03-11 15:25:00 +0000

    all variables and constructor created

---
+++ src/cycling/Stage.java
@@ -0,0 +1,29 @@
+package cycling;
+import java.time.LocalTime;
+import java.util.HashMap;
+
+public class Stage {
+    private int stageID;
+    private int raceID;
+    private String stageName;
+    private String description; 
+    private double length;
+    private LocalTime startTime;
+    private StageType stageType;
+    private HashMap<Integer, String> checkpointIDHashMap = new HashMap<Integer, String>();
+    private LocalTime[] totalTimes;
+    private static int nextStageID = 0;
+
+    public int Stage(int raceID, String stageName, String description, Double length, LocalTime startTime, StageType StageType)
+    {
+        this.raceID = raceID;
+        this.stageName = stageName;
+        this.description = description;
+        this.length = length;
+        this.startTime = startTime;
+        this.stageType = stageType;
+        this.stageID = nextStageID++;
+
+        return stageID;
+    }
+}
