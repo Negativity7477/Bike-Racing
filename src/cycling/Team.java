4fb1167f887548f21b3dc3e0a89ea9631e3aeb2b
ColumXB <columxb@gmail.com>
2024-03-17 16:24:42 +0000

    Team has been commented, all code is yet to be tested

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -19,7 +19,12 @@ public class Team {
         return teamID;
     }
 
-    // Gets an array of all the rider's IDs in the team
+    /**
+     * Gets an array of all the rider's IDs in the team, 
+     * If there are no riders than an empty array is returned
+     * 
+     * @return An array of the IDs of all the riders in the team being queried
+     */
     public int[] getRiderIDArray() {
 
         int[] riderIDArray = new int[ridersHash.size()];
@@ -33,24 +38,43 @@ public class Team {
         return riderIDArray;
     }
 
-    // Adds Rider object as a value in ridersHash with a key of its Rider's ID
+    /**
+     * Adds Rider object as a value in ridersHash with a key of its Rider's ID
+     * 
+     * @param newRider A Rider object to be added to the rider hashmap
+     * 
+     * @throws DuplicatedResultException If the rider being added has already been added
+     */
     public void addRider(Rider newRider) throws DuplicatedResultException
     {
 
         Integer riderID = newRider.getRiderID();
 
         if (ridersHash.put(riderID, newRider) != null) {
-            throw new DuplicatedResultException("Bad ID");
+            throw new DuplicatedResultException("This rider has already been added to this team");
         } 
     }
 
-    // Removes Rider object reference which then deletes the object
-    public void deleteRider(int riderID) {
+    /**
+     * Removes Rider object reference which then deletes the object
+     * 
+     * @param riderID The ID of the rider to be removed from the rider hash
+     * 
+     * @throws IDNotRecognisedException If the rider being removed is not in rider hashmap
+     */
+    public void deleteRider(int riderID) throws IDNotRecognisedException {
 
-        ridersHash.remove(riderID);
+        if (ridersHash.remove(riderID) == null) {
+            throw new IDNotRecognisedException("This rider is not in this team");
+        }
     }
 
-    // Constructor
+    /**
+     * Constructor
+     * 
+     * @param name A name for the team
+     * @param description A description for the team
+     */
     public Team(String name, String description) {
 
         this.name = name;
